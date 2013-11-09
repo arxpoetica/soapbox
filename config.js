aef2c304eacdc8516824c09bd6d1e8d35d0c6dfc
Matt Warren <mwarren@wanderu.com>
2013-11-08 23:12:12 -0500

    more environment hotness

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var isProduction = (process.env.NODE_ENV === 'production');
 var rootDir = isProduction === true ? '/home/deploy/current' : process.cwd();
 	nconf = require('nconf'),
 	fs = require('fs'),
-	env = require(rootDir + '/bin/server').app.get('env'),
+	env = isProduction === true ? require(rootDir + 'server.js').app.get('env) : require(rootDir + '/bin/server').app.get('env'),
 	configFilename = env !== 'development' ? '/config_' + env + '.json' : '/config.json',
 	json = JSON.parse(fs.readFileSync(rootDir + '/package.json', 'utf8'));
 
